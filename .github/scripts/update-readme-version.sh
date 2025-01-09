4e7ada766280ece21a3a74931bcc42eafdc2e18b
Kazik Pogoda <morisil@xemantic.com>
2025-01-09 12:30:36 +0100

    copyright update to include https link to the original license (#6)

--- .github/scripts/update-readme-version.sh
+++ .github/scripts/update-readme-version.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #
-# Copyright 2024 Kazimierz Pogoda / Xemantic
+# Copyright 2024-2025 Kazimierz Pogoda / Xemantic
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#     https://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
